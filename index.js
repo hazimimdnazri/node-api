93002eb8f2593ddf5b3b5934066941b2479c773b
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2022-02-22 21:52:27 +0800

    GENESIS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const app = express()
 require('dotenv').config()
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
@@ -22,9 +22,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/validate', (req, res) => {
-    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-    console.log(`POST request from ${ip} with data: ${JSON.stringify(req.body)}`)
-    res.send('POST request sent successfully')
+    let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?'
+    let query = mysql.format(selectQuery,["users","sn", req.body.serial_number])
+    connection.query(query,(err, data) => {
+        if(err){
+            console.error(err)
+            return
+        }
+        if(Object.keys(data).length !== 0  ){
+            if(data[0].computer_id != req.body.machine_id){
+                res.status(200).json({status : 'error', message : 'The serial number is not registered to this machine.'})
+            } else {
+                res.status(200).json({status : 'success', message : 'The serial number is registered.'})
+            }
+        } else {
+            res.status(200).json({status : 'error', message : 'The serial number does not exist.'})
+        }
+    });
 })
 
 app.get('*', (req, res) => {
